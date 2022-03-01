11f9e710c5299545c864b272849951566f83072a
arista <atd-help@arista.com>
2022-03-01 18:44:17 +0000

    new script

---
+++ version_check.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from cvprac.cvp_client import CvpClient
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+cvp1 = "192.168.0.5"
+cvp_user = "arista"
+cvp_pw = "aristarsc2" # Change this to your environment password
+
+client = CvpClient()
+
+client.connect([cvp1], cvp_user, cvp_pw)
+
+result = client.api.get_inventory()
+
+print(result)
+
+for item in result:
+    print(item['hostname'], 'is running version', item['version'])
