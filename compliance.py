1264b046f1f8c0f9276d7d37e3d63f661c5ceb3a
arista <atd-help@arista.com>
2022-03-01 17:39:53 +0000

    First commit

---
+++ compliance.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from cvprac.cvp_client import CvpClient
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+cvp1 = "192.168.0.5"
+cvp_user = "arista"
+cvp_pw = "aristarsc2"
+
+client = CvpClient()
+
+client.connect([cvp1], cvp_user, cvp_pw)
+
+result = client.api.get_cvp_info()
+print(result)
